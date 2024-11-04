4b581a66679def00039c32d03a03a6e5b01fc128
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2024-11-04 22:53:34 +0000

    :construction: We added support for details in CardPeople

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from "react"
+import React, { useState, useEffect, useContext } from "react"
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import CardPeople from "../component/cardPeople";
@@ -8,58 +8,78 @@ import CardVehicles from "../component/cardVehicle"
 export const Home = () => {
 
 	const [people, setPeople] = useState([])
-    const [planet, setPlanet] = useState([])
+	const [planet, setPlanet] = useState([])
 	const [vehicle, setVehicle] = useState([])
 
-	useEffect(()=>{
-		fetch("https://www.swapi.tech/api/people")
-.then(res => res.json())
-.then(data => setPeople(data.results))
-.catch(err => console.error(err));
+	const loadPeopleWithDetails = async () => {
+		const newPeopleList = []
+		const response = await fetch("https://www.swapi.tech/api/people")
+		const data = await response.json()
 
-fetch("https://www.swapi.tech/api/planets")
-.then(res => res.json())
-.then(data => setPlanet(data.results))
-.catch(err => console.error(err));
+		for (let people of data.results) {
+			const resPeople = await fetch(people.url);
+			const peopleDetails = await resPeople.json();
 
-fetch("https://www.swapi.tech/api/vehicles")
-.then(res => res.json())
-.then(data => setVehicle(data.results))
-.catch(err => console.error(err));
+			console.log(peopleDetails)
+			newPeopleList.push(peopleDetails.result);
+		}
 
+		setPeople(newPeopleList);
 	}
-	,[])
+
+	useEffect(() => {
+
+		loadPeopleWithDetails()
+
+		fetch("https://www.swapi.tech/api/planets")
+			.then(res => res.json())
+			.then(data => setPlanet(data.results))
+			.catch(err => console.error(err));
+
+		fetch("https://www.swapi.tech/api/vehicles")
+			.then(res => res.json())
+			.then(data => setVehicle(data.results))
+			.catch(err => console.error(err));
+
+	}, [])
 
 	return <div className="text-center mt-5">
 		<h1>Warriors</h1>
 		<div className="d-flex flex-row overflow-auto">
-		{
-			people.map((item,index)=>{
-				return (
-					<CardPeople key={index} name={item.name} uid={item.uid}> </CardPeople>
-				)
-			})
-		}
+
+			{
+				people.length == 0 && <div className="spinner-border" role="status">
+					<span className="visually-hidden">Loading...</span>
+				</div>
+			}
+
+			{
+				people.map((item, index) => {
+					return (
+						<CardPeople key={index} item={item.properties} uid={item.uid}> </CardPeople>
+					)
+				})
+			}
 		</div>
 		<h1>Planets</h1>
 		<div className="d-flex flex-row overflow-auto">
-		{
-			planet.map((item,index)=>{
-				return (
-					<CardPlanets key={index} name={item.name} uid={item.uid}></CardPlanets>
-				)
-			})
-		}
+			{
+				planet.map((item, index) => {
+					return (
+						<CardPlanets key={index} name={item.name} uid={item.uid}></CardPlanets>
+					)
+				})
+			}
 		</div>
 		<h1>Vehicles</h1>
 		<div className="d-flex flex-row overflow-auto">
-		{
-			vehicle.map((item,index)=>{
-				return (
-					<CardVehicles key={index} name={item.name} uid={item.uid}></CardVehicles>
-				)
-			})
-		}
+			{
+				vehicle.map((item, index) => {
+					return (
+						<CardVehicles key={index} name={item.name} uid={item.uid}></CardVehicles>
+					)
+				})
+			}
 		</div>
 	</div>
 
